6df547cd67ebcda1c1515fd6002063d08160324d
Anisul Islam <anisulislam@Anisuls-MacBook-Pro.local>
2022-04-05 18:10:45 +0600

    minimal nav

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
     <div>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
       </Routes>
     </div>
   );
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -3,18 +3,16 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav className="flex flex-nowrap h-10 place-content-between w-full p-3">
-      <Link to={"/home"}>Home Decor</Link>
-      <div className="place-content-around">
-        <Link to={"/home"}>Shop</Link>
-        <Link to={"/aboutus"}>About Us</Link>
-        <Link to={"/contactus"}>Contact Us</Link>
-      </div>
-      <div className="place-content-around">
-        <Link to={"/cart"}>Shop</Link>
-        <Link to={"/wishlist"}>WISHLIST</Link>
-        <Link to={"/Login"}>Login</Link>
-      </div>
+    <nav className="flex flex-nowrap place-content-evenly w-full h-12 items-center">
+      <Link className="text-4xl" to={"/home"}>
+        Home Decor
+      </Link>
+      <Link to={"/home"}>Shop</Link>
+      <Link to={"/aboutus"}>About Us</Link>
+      <Link to={"/contactus"}>Contact Us</Link>
+      <Link to={"/cart"}>Cart</Link>
+      <Link to={"/wishlist"}>WISHLIST</Link>
+      <Link to={"/Login"}>Login</Link>
     </nav>
   );
 };
