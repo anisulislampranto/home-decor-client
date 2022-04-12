bdcabfa62ae92a5d30bf9318f7fdb44aa6b08b53
Anisul Islam <anisulislam@Anisuls-MacBook-Pro.local>
2022-04-12 17:08:57 +0600

    font added

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,18 +1,46 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { faHeart } from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faArrowRightFromBracket,
+  faBars,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
   return (
-    <nav className="flex flex-nowrap place-content-evenly w-full h-12 items-center">
-      <Link className="text-4xl" to={"/home"}>
-        Home Decor
-      </Link>
-      <Link to={"/home"}>Shop</Link>
-      <Link to={"/aboutus"}>About Us</Link>
-      <Link to={"/contactus"}>Contact Us</Link>
-      <Link to={"/cart"}>Cart</Link>
-      <Link to={"/wishlist"}>WISHLIST</Link>
-      <Link to={"/Login"}>Login</Link>
+    <nav className="sm:flex flex place-content-evenly w-full items-center">
+      <div className="space-x-56 items-center justify-center">
+        <Link className="text-4xl font-bold my-auto" to={"/home"}>
+          Home Decor
+        </Link>
+        <button className="lg:hidden sm:hidden hover:bg-slate-300 p-2 rounded my-auto">
+          <FontAwesomeIcon icon={faBars} />
+        </button>
+      </div>
+      <div className="hidden flex-col sm:block sm:space-x-20">
+        <Link className="hover:text-gray-500" to={"/shop"}>
+          Shop
+        </Link>
+        <Link className="hover:text-gray-500" to={"/aboutus"}>
+          About Us
+        </Link>
+        <Link className="hover:text-gray-500" to={"/contactus"}>
+          Contact Us
+        </Link>
+        {/* <div className="space-x-8"> */}
+        <Link className="hover:text-gray-500" to={"/cart"}>
+          Cart($0)
+        </Link>
+        <Link className="hover:text-gray-500" to={"/wishlist"}>
+          <FontAwesomeIcon icon={faHeart} />
+          (0)
+        </Link>
+        <Link className="hover:text-gray-500" to={"/Login"}>
+          <FontAwesomeIcon icon={faArrowRightFromBracket} /> Login
+        </Link>
+        {/* </div> */}
+      </div>
     </nav>
   );
 };
