47f185a1b499eefda5d9d2efeb0bafdfeb13f065
Anisul Islam <anisulislam@Anisuls-MacBook-Pro.local>
2022-04-15 02:41:27 +0600

    animated sliding image added

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,11 +1,40 @@
 import React from "react";
 import Navbar from "../Navbar/Navbar";
+import image from "../../images/jean-philippe-delberghe-Ry9WBo3qmoc-unsplash-removebg-preview.png";
+import "./Header.css";
+import imageTwo from "../../images/sam-moqadam-kvmdsTrGOBM-unsplash-removebg-preview.png";
+import imageThree from "../../images/mutzii-vHLa285gvcU-unsplash-removebg-preview.png";
 
 const Header = () => {
   return (
-    <div>
+    <>
       <Navbar />
-    </div>
+      <div className=" bg-gray-200 h-96 w-full flex justify-evenly">
+        <div className="my-auto">
+          <h1 className="justify-center text-4xl sm:text-7xl">
+            Think Defferent
+          </h1>
+          <p className="text-xl sm:text-4xl">
+            Lorem ipsum dolor sit <br /> amet consectetur adipisicing <br />
+            elit. Quo, ab.
+          </p>
+        </div>
+        {/* <div>
+          <img className="animate-pulse" src={image} alt="" />
+        </div> */}
+        <div style={{ height: "100%", width: "40%" }} id="slideset1">
+          <div className="w-full sm:w-5/6 my-auto">
+            <img id="image-one" className="animate-pulse" src={image} alt="" />
+          </div>
+          <div className="w-full my-10">
+            <img className="" src={imageTwo} alt="" />
+          </div>
+          <div className="w-full my-10">
+            <img className="" src={imageThree} alt="" srcset="" />
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -9,39 +9,41 @@ import {
 
 const Navbar = () => {
   return (
-    <nav className="sm:flex flex place-content-evenly w-full items-center">
-      <div className="space-x-56 items-center justify-center">
-        <Link className="text-4xl font-bold my-auto" to={"/home"}>
-          Home Decor
-        </Link>
-        <button className="lg:hidden sm:hidden hover:bg-slate-300 p-2 rounded my-auto">
-          <FontAwesomeIcon icon={faBars} />
-        </button>
-      </div>
-      <div className="hidden flex-col sm:block sm:space-x-20">
-        <Link className="hover:text-gray-500" to={"/shop"}>
-          Shop
-        </Link>
-        <Link className="hover:text-gray-500" to={"/aboutus"}>
-          About Us
-        </Link>
-        <Link className="hover:text-gray-500" to={"/contactus"}>
-          Contact Us
-        </Link>
-        {/* <div className="space-x-8"> */}
-        <Link className="hover:text-gray-500" to={"/cart"}>
-          Cart($0)
-        </Link>
-        <Link className="hover:text-gray-500" to={"/wishlist"}>
-          <FontAwesomeIcon icon={faHeart} />
-          (0)
-        </Link>
-        <Link className="hover:text-gray-500" to={"/Login"}>
-          <FontAwesomeIcon icon={faArrowRightFromBracket} /> Login
-        </Link>
-        {/* </div> */}
-      </div>
-    </nav>
+    <div className="p-2">
+      <nav className="sm:flex flex place-content-evenly w-full items-center">
+        <div className="space-x-56 items-center justify-center">
+          <Link className="text-4xl font-bold my-auto" to={"/home"}>
+            Home Decor
+          </Link>
+          <button className="lg:hidden sm:hidden hover:bg-slate-300 p-2 rounded my-auto">
+            <FontAwesomeIcon icon={faBars} />
+          </button>
+        </div>
+        <div className="hidden flex-col sm:block sm:space-x-20">
+          <Link className="hover:text-gray-500" to={"/shop"}>
+            Shop
+          </Link>
+          <Link className="hover:text-gray-500" to={"/aboutus"}>
+            About Us
+          </Link>
+          <Link className="hover:text-gray-500" to={"/contactus"}>
+            Contact Us
+          </Link>
+          {/* <div className="space-x-8"> */}
+          <Link className="hover:text-gray-500" to={"/cart"}>
+            Cart($0)
+          </Link>
+          <Link className="hover:text-gray-500" to={"/wishlist"}>
+            <FontAwesomeIcon icon={faHeart} />
+            (0)
+          </Link>
+          <Link className="hover:text-gray-500" to={"/Login"}>
+            <FontAwesomeIcon icon={faArrowRightFromBracket} /> Login
+          </Link>
+          {/* </div> */}
+        </div>
+      </nav>
+    </div>
   );
 };
 
